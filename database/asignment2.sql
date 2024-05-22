8558d1e25b29d70663a7efd296928230f57fea53
RebecaMurcia <lop17010@byui.edu>
2024-05-21 19:16:09 -0700

    w5

--- database/asignment2.sql
+++ database/asignment2.sql
@@ -2,3 +2,7 @@ INSERT INTO account
 ( account_firstname , account_lastname , account_email , account_password)
 VALUES
 ( 'Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+UPDATE account
+SET
+account_type = 'Admin'
